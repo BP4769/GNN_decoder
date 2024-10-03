96479aa5daf69ee67ada33c7eee3ba842b7f5074
BP4769 <57891977+BP4769@users.noreply.github.com>
2024-10-03 11:57:08 +0200

    Added notebook for testing

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+# src/__init__.py
+
+# This file can be empty or you can use it to initialize the package.
+# For example, you can import specific classes or functions here.
+
+from .GNN_Decoder import GNN_Decoder
